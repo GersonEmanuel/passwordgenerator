be3d318996a617525e94a9a9488b78bf45460147
GersonEmanuel <gersonemanuel432@gmail.com>
2025-04-06 12:07:01 -0300

    reset, style css

---
+++ scripts.js
@@ -0,0 +1,31 @@
+let passwordleng = 16;
+const inputpasswordEl = document.querySelector("#password")
+
+
+function generatepassword() {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@[]^_{|}~"
+
+    let password = ""
+
+    for(let i = 0; i<passwordleng; i++){
+        const randomnumber = Math.floor(Math.random()*chars.length)
+        password += chars.substring(randomnumber, randomnumber + 1)
+
+    }
+    inputpasswordEl.value = password
+}
+
+function copy() {
+    navigator.clipboard.writeText(inputpasswordEl.value)
+}
+
+const passwordlengthEL = document.querySelector("#password-length")
+passwordlengthEL.addEventListener("input", function(){
+    const passwordleng = passwordlengthEL.value
+    generatepassword()
+})
+
+const copybuttonEl = document.querySelector("#copy")
+copybuttonEl.addEventListener("click", copy)
+
+generatepassword()
