8bc77d4ec7380fbcdd23b67db047acf28ce1e66c
GersonEmanuel <gersonemanuel432@gmail.com>
2025-04-06 15:02:34 -0300

    checkbox properties

--- scripts.js
+++ scripts.js
@@ -2,12 +2,12 @@ let passwordleng = 16;
 const inputpasswordEl = document.querySelector("#password")
 
 
-function generatepassword() {
+function generatepassword(passwordlength) {
     const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&()*+,-./:;<=>?@[]^_{|}~"
 
     let password = ""
 
-    for(let i = 0; i<passwordleng; i++){
+    for(let i = 0; i<passwordlength; i++){
         const randomnumber = Math.floor(Math.random()*chars.length)
         password += chars.substring(randomnumber, randomnumber + 1)
 
@@ -21,11 +21,11 @@ function copy() {
 
 const passwordlengthEL = document.querySelector("#password-length")
 passwordlengthEL.addEventListener("input", function(){
-    const passwordleng = passwordlengthEL.value
-    generatepassword()
+    const passwordlength = passwordlengthEL.value
+    generatepassword(passwordlength)
 })
 
 const copybuttonEl = document.querySelector("#copy")
 copybuttonEl.addEventListener("click", copy)
 
-generatepassword()
+generatepassword(passwordlength)
